ba229bdaf5284aeae017698b48cc51af3c6e8f86
Diaa <diaa.mohamed91997@gmail.com>
2022-06-27 16:57:31 +0200

    update with the newest version of cypress

--- src/helper/readConfig.ts
+++ src/helper/readConfig.ts
@@ -2,18 +2,21 @@ import fs from 'fs'
 import { TestrailReporterConfig } from '../types'
 
 export default function readConfig(): TestrailReporterConfig | null {
-  try {
-    const config = fs.readFileSync(`${process.cwd()}/cypress.json`, 'utf-8')
-    const cypressConfig = JSON.parse(config)
-
-    return {
-      ...cypressConfig.testrailReporter,
-      reportFilename: cypressConfig.testrailReporter?.reportFilename,
+  var _a;
+  try {   
+      if (fs_1.default.existsSync(`${process.cwd()}/cypress.json`)) {
+          const config = fs_1.default.readFileSync(`${process.cwd()}/cypress.json`, 'utf-8');
+          const cypressConfig = JSON.parse(config);
+    }else{
+      const config = fs_1.default.readFileSync(`${process.cwd()}/testrailConfig.json`, 'utf-8');
+      const cypressConfig = JSON.parse(config);
     }
-  } catch (e) {
-    // eslint-disable-next-line no-console
-    console.error(`No cypress.json config found at ${process.cwd()}`)
-    process.exit(1)
+      return Object.assign(Object.assign({}, cypressConfig.testrailReporter), { reportFilename: (_a = cypressConfig.testrailReporter) === null || _a === void 0 ? void 0 : _a.reportFilename });
   }
-  return null
+  catch (e) {
+      // eslint-disable-next-line no-console
+      console.error(` ${e}`);
+      process.exit(1);
+  }
+  return null;
 }
